66dca7baedcd66dcab5fe3eec98a4142d22b168f
Jorge Carrera <Jorge Carrera>
2017-04-03 20:36:20 -0700

    Exercise 1, v1.0.0.2, EchoServer and EchoClient working

--- EchoClient.java
+++ EchoClient.java
@@ -3,22 +3,30 @@ import java.io.InputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.io.OutputStream;
+import java.io.PrintStream;
 
 public final class EchoClient {
 
     public static void main(String[] args) throws Exception {
         try (Socket socket = new Socket("localhost", 22222)) {
-            String user;
+            String user = "";
             
             InputStream is = socket.getInputStream();
             InputStreamReader isr = new InputStreamReader(is, "UTF-8");
-            BufferedReader br = new BufferedReader(isr);
-            System.out.println(br.readLine());
+            BufferedReader bur = new BufferedReader(isr);
+            System.out.println(bur.readLine());
             InputStreamReader in = new InputStreamReader(System.in);
             BufferedReader brin = new BufferedReader(in);
-            
-            while((user = brin.readLine()) != "exit"){
-                System.out.println("Server> " + br.readLine());
+            OutputStream out = socket.getOutputStream();
+            PrintStream output = new PrintStream(out, true, "UTF-8");
+
+            System.out.print("Client> ");            
+            while(!"exit".equals(user)){
+                user = brin.readLine();
+                output.println(user);
+                System.out.println(bur.readLine());
+                System.out.print("Client> "); 
             }
             
             
--- EchoServer.java
+++ EchoServer.java
@@ -3,10 +3,16 @@ import java.io.OutputStream;
 import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public final class EchoServer {
 
     public static void main(String[] args) throws Exception {
+        boolean exitNotHit = true;
+        String message;
+        
         try (ServerSocket serverSocket = new ServerSocket(22222)) {
             while (true) {
                 try (Socket socket = serverSocket.accept()) {
@@ -15,6 +21,22 @@ public final class EchoServer {
                     OutputStream os = socket.getOutputStream();
                     PrintStream out = new PrintStream(os, true, "UTF-8");
                     out.printf("Hi %s, thanks for connecting!%n", address);
+                    
+                    InputStream is = socket.getInputStream();
+                    InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+                    BufferedReader br = new BufferedReader(isr);
+                    
+                    while(exitNotHit){
+                        message = br.readLine();
+                        out.println("Server> " + message);
+                        
+                        if("exit".equals(message)){
+                            exitNotHit = false;
+                            System.out.printf("Client disconnected: %s%n", address);
+                        }
+                        
+                    }
+                    
                 }
             }
         }
